4a65d7e8fdd5cfeec5d8a93f906aec8f47acd5de
Sergio Tomey <sergiotomey96@hotmail.com>
2021-03-11 21:00:36 -0300

    Desafio 2

---
+++ illusion-calzados/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const NavBar = () => {
+    return (
+        <div>
+        <h1>Illusion Calzados</h1>
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Catalogo</a></li>
+            <li><a href="">Sobre Nosotros</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </div>
+    )
+}
